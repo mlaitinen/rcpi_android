bb1407968f880990ad86ea9581afa0a4dbc845e5
Miku Laitinen <miku.laitinen@gmail.com>
2014-05-10 16:43:26 +0300

    Wifi signal is now visible. Maximum steering angle can be set. Engine and steering can be disabled.

--- UDPCarController/src/main/java/fi/ninjaware/udpcarcontroller/TiltTouchActivity.java
+++ UDPCarController/src/main/java/fi/ninjaware/udpcarcontroller/TiltTouchActivity.java
@@ -1,26 +1,33 @@
 package fi.ninjaware.udpcarcontroller;
 
 import android.app.Activity;
-import android.content.res.Resources;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.LayerDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.widget.ImageView;
-import android.widget.SeekBar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 
 public class TiltTouchActivity extends Activity implements SensorEventListener {
+
+    private enum ControlMode {
+        BOTH,
+        STEERING,
+        ENGINE
+    }
 
     private static final String TAG = TiltTouchActivity.class.getName();
 
@@ -38,6 +45,8 @@ public class TiltTouchActivity extends Activity implements SensorEventListener {
 
     private byte previousTurn = 0;
 
+    private float previousPitch = 0.0f;
+
     private byte previousAccel = 0;
 
     private ArrowImageView mImageAccel;
@@ -50,11 +59,53 @@ public class TiltTouchActivity extends Activity implements SensorEventListener {
 
     private TextView mTextTurn;
 
+    private ScheduledFuture<?> wifiSignalSchedule;
+
+    private ControlMode currentControlMode = ControlMode.BOTH;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_accel);
+
+        ((Button) findViewById(R.id.button_max_angle)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float pitch = Math.abs(previousPitch);
+                angleFactor = 100 / pitch;
+
+                Toast.makeText(TiltTouchActivity.this, String.format("Max angle: %.2f", pitch),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Button controlButton = ((Button) findViewById(R.id.button_controls_enabled));
+        controlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* Cycle:
+                    1. Engine + steering
+                    2. Steering
+                    3. Engine
+                    */
+
+                switch(currentControlMode) {
+                    case BOTH:
+                        currentControlMode = ControlMode.STEERING;
+                        controlButton.setText("Steering");
+                        break;
+                    case STEERING:
+                        currentControlMode = ControlMode.ENGINE;
+                        controlButton.setText("Engine");
+                        break;
+                    case ENGINE:
+                        currentControlMode = ControlMode.BOTH;
+                        controlButton.setText("Eng + steer");
+                        break;
+                }
+            }
+        });
     }
 
     @Override
@@ -62,11 +113,30 @@ public class TiltTouchActivity extends Activity implements SensorEventListener {
         super.onStart();
 
         // Get the wifi manager.
-        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        final WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
 
-        // Fill in the access point name.
-        ((TextView) findViewById(R.id.text_accesspoint)).setText(
-                "Access point: " + wm.getConnectionInfo().getSSID());
+        // Check the wifi signal strength every second
+        ScheduledExecutorService wifiSignalService = Executors.newSingleThreadScheduledExecutor();
+        wifiSignalSchedule = wifiSignalService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        WifiInfo info = wm.getConnectionInfo();
+                        int strength = WifiManager.calculateSignalLevel(info.getRssi(), 100);
+                        String strengthStr = String.format("Signal strength: %d", strength);
+
+                        // Fill in the signal strength.
+                        ((TextView) findViewById(R.id.text_signal_strength)).setText(strengthStr);
+
+                        // Fill in the access point name.
+                        ((TextView) findViewById(R.id.text_accesspoint)).setText(
+                                "Access point: " + info.getSSID());
+                    }
+                });
+            }
+        }, 0, 1, TimeUnit.SECONDS);
 
         // Create a new UDP message dispatcher.
         mDispatcher = new MessageDispatcher(wm);
@@ -91,6 +161,7 @@ public class TiltTouchActivity extends Activity implements SensorEventListener {
     protected void onStop() {
         super.onStop();
         mDispatcher.close();
+        wifiSignalSchedule.cancel(false);
     }
 
 
@@ -101,6 +172,10 @@ public class TiltTouchActivity extends Activity implements SensorEventListener {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
+        if(currentControlMode.equals(ControlMode.STEERING)) {
+            return true;
+        }
 
         int pointerId = event.getPointerId(0);
         if(mPointerId != -1 && pointerId != mPointerId) {
@@ -156,6 +231,10 @@ public class TiltTouchActivity extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
 
+        if(currentControlMode == ControlMode.ENGINE) {
+            return;
+        }
+
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             mGravity = event.values.clone();
         }
@@ -172,11 +251,12 @@ public class TiltTouchActivity extends Activity implements SensorEventListener {
             SensorManager.getOrientation(R, orientation);
 
             byte turn = convertPitchToTurn(orientation[1]);
+            previousPitch = orientation[1];
             if(turn != previousTurn) {
 
                 mDispatcher.sendMessage(new ControlEvent(ControlType.TURN, turn));
 
-                mTextTurn.setText(String.format("Turn: %d", turn));
+                mTextTurn.setText(String.format("Steering: %d", turn));
                 mImageTurn.setMagnitude(turn);
                 mImageTurn.invalidate();
 
@@ -185,8 +265,10 @@ public class TiltTouchActivity extends Activity implements SensorEventListener {
         }
     }
 
+    private float angleFactor = 150.0f;
+
     private byte convertPitchToTurn(float rawPitch) {
-        int turn = Math.round(rawPitch*150.0f);
+        int turn = Math.round(rawPitch* angleFactor);
 
         if(turn > 100) turn = 100;
         else if(turn < -100) turn = -100;
